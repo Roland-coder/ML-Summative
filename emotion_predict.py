f9db4d384f62fbd94f3baee421dbc697693b229b
Neba Roland <53044894+Roland-coder@users.noreply.github.com>
2021-12-09 09:58:58 +0200

    Update emotion_predict.py

--- emotion_predict.py
+++ emotion_predict.py
@@ -94,7 +94,7 @@ X_train, X_val, y_train, y_val, word_dict = tokenizer(df.Text, df.Label, new_tex
 label_back = encoder.classes_
 
 if st.button('Predict Overall Performance'):
-	pred = model.predict(X_val)[4292]
+	pred = model.predict(X_val)
 	pred = np.argmax(pred, axis = 0)
 	if pred == 0:
 		st.write("Tweet emotion is ANGER")
